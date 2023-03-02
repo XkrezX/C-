f8983db5d917711fa3b4810a9c3358ad111fa8e6
XkrezX <113830080+XkrezX@users.noreply.github.com>
2023-03-02 12:41:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Дана строка символов. Необходимо проверить, является ли эта строка палиндромом.
+        Console.Write("Введите строку: ");
+        string str = Console.ReadLine();
+
+       
+        bool isPalindrome = true;
+        for (int i = 0; i < str.Length / 2; i++)
+        {
+            if (str[i] != str[str.Length - i - 1])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+      
+        if (isPalindrome)
+        {
+            Console.WriteLine("Строка является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Строка не является палиндромом");
+        }
+
+        Console.ReadKey();
+        #endregion
+    }
+}
